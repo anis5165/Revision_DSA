ea5eb7890a5be7cdd3d0b17bcb2a43f3cb1fa15e
Anish Kumar <anishkumargvm02@gmail.com>
2025-01-24 10:39:41 +0530

    passing array to function and pointer

---
+++ Arrays/pointerAndArray.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {4,2,6,1,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int* ptr = arr;     //giving address of the first element of an array
+    for(int i=0;i<n;i++){
+        cout<<ptr<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<ptr[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<i[ptr]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<i[arr]<<" ";
+    }
+
+}
