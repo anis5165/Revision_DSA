3c4146243d69e4942bf222012fdd86a7584492ae
Anish Kumar <anishkumargvm02@gmail.com>
2025-01-28 11:07:22 +0530

    vector basics

---
+++ Vector/VectorBasics.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v;
+    v.push_back(6);
+    cout<<"Size: "<<v.size()<<endl;
+    cout<<"Capacity: "<<v.capacity()<<endl;
+    v.push_back(1);
+    cout<<"Size: "<<v.size()<<endl;
+    cout<<"Capacity: "<<v.capacity()<<endl;
+    v.push_back(9);
+    cout<<"Size: "<<v.size()<<endl;
+    cout<<"Capacity: "<<v.capacity()<<endl;
+    v.push_back(0);
+    cout<<"Size: "<<v.size()<<endl;
+    cout<<"Capacity: "<<v.capacity()<<endl;
+
+
+    cout<<endl;
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    
+}
